d1b864ded9ac1b69fd60fa285b3dfed65df4e0ca
RedFlame <53618581+huynhat30@users.noreply.github.com>
2023-08-03 23:15:57 +0100

    Styling navigation bar

--- my-portfolio/src/components/SidebarNav/index.js
+++ my-portfolio/src/components/SidebarNav/index.js
@@ -1,15 +1,53 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
-import LogoBig from '../../assets/images/logo-s.png'
-import LogoSmall from '../../assets/images/logo_sub.png'
+import { Link, NavLink } from 'react-router-dom'
+import LogoBig from '../../assets/images/profile.jpg'
+import LogoSmall from '../../assets/images/name_logo2.png'
+import { FontAwesomeIcon }  from '@fortawesome/react-fontawesome'
+import { faCogs, faEnvelope, faHome, faMugHot, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
     return (
     <div className="nav-bar">
         <Link className='logo' to='/'>
-            <img alt='front page logo' src={LogoBig} />
-            <img alt='small logo' src={LogoSmall} />
+            <img alt='front page logo' src={LogoBig} className='profile-logo'/>
+            <img alt='small logo' src={LogoSmall} className='name-logo'/>
         </Link>
+
+        <nav>
+            <NavLink id='link' exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} className='icon'/>
+            </NavLink>
+
+            <NavLink id='link' exact='true' activeclassname='active' className='about' to='/about'>
+                <FontAwesomeIcon icon={faUser} className='icon'/>
+            </NavLink>
+
+            <NavLink id='link' exact='true' activeclassname='active' className='skill' to='/skill'>
+                <FontAwesomeIcon icon={faCogs} className='icon'/>
+            </NavLink>
+
+            <NavLink id='link' exact='true' activeclassname='active' className='project' to='/projects'>
+                <FontAwesomeIcon icon={faMugHot} className='icon'/>
+            </NavLink>
+
+            <NavLink id='link' exact='true' activeclassname='active' className='contact' to='/contact'>
+                <FontAwesomeIcon icon={faEnvelope} className='icon'/>
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a href="https://www.linkedin.com/in/nhat-huy-giang-50abb7201" target='_blank' rel='noreferrer'>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/huynhat30" target='_blank' rel='noreferrer'>
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+            </li>
+        </ul>
     </div>)
 }
 
